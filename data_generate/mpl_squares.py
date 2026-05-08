2a71a2aa2d5921a7cda3092c2d88101d60a8c469
harlock <xarlokua@gmail.com>
2026-05-08 21:29:20 +0300

    08.05

--- data_generate/mpl_squares.py
+++ data_generate/mpl_squares.py
@@ -1,8 +1,19 @@
 import matplotlib.pyplot as plt
 
+
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9 , 16, 25]
 
+plt.style.use('seaborn-v0_8-white')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares , linewidth=3)
+
+# Задати назву для графіка так кожної з осей.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Задати розмір шрифту для підписів на розмітці.
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
---
+++ data_generate/scatter_squares.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+
+# Задати назву для графіка та кожної з осей.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Задати розмір шрифту для підписів на розмітці.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Задати діапазон для кожної з осей 
+ax.axis([0, 1100, 0, 11_00_000])
+plt.show()
+#plt.savefig('squares_plot.png', bbox_inches='tight')
