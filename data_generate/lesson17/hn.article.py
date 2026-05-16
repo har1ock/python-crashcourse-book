73123dddf3a916fcfd6d0f9ca395bd50f806aeae
harlock <xarlokua@gmail.com>
2026-05-16 21:50:24 +0300

    16.05

---
+++ data_generate/lesson17/hn.article.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+# Зробити виклик через API та зберегти відповідь.
+url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# Дослідити структуру даних.
+response_dict = r.json()
+readeble_file = 'data_generate/lesson17/readeble_hn_data.json'
+with open(readeble_file, 'w') as f:
+    json.dump(response_dict, f, indent=4)
